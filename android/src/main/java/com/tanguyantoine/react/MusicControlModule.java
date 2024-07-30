a216af909a7fcdecc8979d158255f2e567b86351
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2024-07-30 13:01:25 +0200

    Add receiver

--- android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
@@ -207,10 +207,10 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
         receiver = new MusicControlReceiver(this, context);
         // context.registerReceiver(receiver, filter);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.registerReceiver(myReceiver, filter, null, null,
+            context.registerReceiver(receiver, filter, null, null,
                     Context.RECEIVER_EXPORTED);
         } else {
-            context.registerReceiver(myReceiver, filter);
+            context.registerReceiver(receiver, filter);
         }
 
         Intent myIntent = new Intent(context, MusicControlNotification.NotificationService.class);
